c59ad96dd6d291ccd9c94294255c04fbd0b7c8c6
9utty <leegh4250@gmail.com>
2023-08-15 20:06:44 +0900

    🌈 수정: 404페이지 수정

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,35 +1,70 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/alt-text */
+import { Grid } from "antd";
 import Link from "next/link";
 import React from "react";
 
+const { useBreakpoint } = Grid;
+
 const NotFound = () => {
+  const screens = useBreakpoint();
+
   return (
-    <div className="bg-indigo-900 fixed overflow-hidden h-[100vh] w-[100vw]">
+    <div
+      style={{
+        backgroundColor: "indigo",
+        position: "fixed",
+        overflow: "hidden",
+        height: "100vh",
+        width: "100vw",
+        zIndex: 1000,
+      }}
+    >
       <img
         src="https://external-preview.redd.it/4MddL-315mp40uH18BgGL2-5b6NIPHcDMBSWuN11ynM.jpg?width=960&crop=smart&auto=webp&s=b98d54a43b3dac555df398588a2c791e0f3076d9"
         style={{ height: "100vh", width: "100vw", objectFit: "cover" }}
       />
       <div className="inset-0 bg-black opacity-25 absolute"></div>
-      <div style={{ zIndex: 10, position: "fixed", top: "50%", left: "65%" }}>
-        <div className="w-full font-mono flex flex-col items-center relative z-10">
+      <div
+        style={{
+          zIndex: 10,
+          position: "fixed",
+          top: screens.sm ? "50%" : "20%",
+          left: screens.sm ? "65%" : "15%",
+        }}
+      >
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            position: "relative",
+            zIndex: 10,
+          }}
+        >
           <h1
-            className="font-extrabold text-5xl text-center text-white leading-tight mt-4"
-            style={{ color: "white", fontSize: "2rem" }}
+            style={{
+              color: "white",
+              fontSize: "2rem",
+              fontWeight: "extrabold",
+              fontFamily: "dunggeunmo-bold",
+            }}
           >
             You are all alone here
           </h1>
-          <p
-            className="font-extrabold text-8xl my-44 text-white animate-bounce"
+          <span
+            className="animate-bounce"
             style={{
+              fontFamily: "dunggeunmo-bold",
               color: "white",
               fontSize: "3rem",
-              transform: "animate-bounce",
+              fontWeight: "extrabold",
               transition: "transform 0.3s",
             }}
           >
             404
-          </p>
+          </span>
         </div>
         <Link href="/">
           <h2
